6d4db682240e53ac9b44786b1a952c0b5060ca2b
wnicolass <nicolaswgl99@gmail.com>
2023-03-06 21:39:11 +0000

    feat: end part 3/c-2

---
+++ web/3-courses/C - Views and Services - 2/services/student_service.py
@@ -0,0 +1,42 @@
+
+from typing import List
+from data.models import Testimonial
+
+
+def student_count() -> int:
+    return 2315
+#:
+
+def get_testimonials(count: int) -> List[Testimonial]:
+    return [
+            Testimonial(
+                user_id = 239,
+                user_name = 'Saul Goodman',
+                user_occupation = 'CEO & Founder',
+                text = 'Quidem HUEHUEHUE odit voluptate, obcaecati, explicabo nobis corporis perspiciatis nihil doloremque eius omnis officia voluptates, facere saepe quas consequatur aliquam unde. Ab numquam reiciendis sequi.',
+            ),
+            Testimonial(
+                user_id = 1001,
+                user_name = 'Sara Wilson',
+                user_occupation = 'Designer',
+                text = 'Export tempor illum tamen malis malis eram quae irure esse labore quem cillum quid cillum eram malis quorum velit fore eram velit sunt aliqua noster fugiat irure amet legam anim culpa.',
+            ),
+            Testimonial(
+                user_id = 704,
+                user_name = 'Jena Karlis',
+                user_occupation = 'Store Owner',
+                text = 'Enim nisi quem export duis labore cillum quae magna enim sint quorum nulla quem veniam duis minim tempor labore quem eram duis noster aute amet eram fore quis sint minim.',
+            ),
+            Testimonial(
+                user_id = 1002,
+                user_name = 'Matt Brandon',
+                user_occupation = 'Freelancer',
+                text = 'Fugiat enim eram quae cillum dolore dolor amet nulla culpa multos export minim fugiat minim velit minim dolor enim duis veniam ipsum anim magna sunt elit fore quem dolore labore illum veniam.',
+            ),
+            Testimonial(
+                user_id = 1589,
+                user_name = 'John Larson',
+                user_occupation = 'Entrepreneur',
+                text = 'Quis quorum aliqua sint quem legam fore sunt eram irure aliqua veniam tempor noster veniam enim culpa labore duis sunt culpa nulla illum cillum fugiat legam esse veniam culpa fore nisi cillum quid.',
+            ),
+        ][:count]
--- web/3-courses/C - Views and Services - 2/views/home.py
+++ web/3-courses/C - Views and Services - 2/views/home.py
@@ -1,120 +1,45 @@
 from fastapi import APIRouter, Request
 from starlette.requests import Request
 from fastapi_chameleon import template
+from common import base_viewmodel_with
+from services import course_service, student_service, trainer_service
+
+POPULAR_COURSES_COUNT = 3
+SELECTED_COURSES_COUNT = 3
+SELECTED_TRAINERS_COUNT = 3
+TESTIMONIALS_COUNT = 5
 
 router = APIRouter()
 
 @router.get('/')                            # type: ignore
 @template()
 async def index(response: Request):
-    return {
-        'num_courses': 99,
-        'num_students': 2315,
-        'num_trainers': 23,
+    return index_viewmodel()
+#:
+
+def index_viewmodel():
+    return base_viewmodel_with({
+        'num_courses': course_service.course_count(),
+        'num_students': student_service.student_count(),
+        'num_trainers': trainer_service.trainer_count(),
         'num_events': 159,
-        'popular_courses': [
-            {
-                'id': 1,
-                'category': 'Hotelaria e Turismo',
-                'price': 169,
-                'name': 'Gestor Turístico', 
-                'summary': 'Et architecto provident deleniti facere repellat nobis iste. Id facere quia quae dolores dolorem tempore.',
-                'trainer_id': 1,
-                'trainer_name': 'Osmar',
-            },
-            {
-                'id': 2,
-                'category': 'Programação em C++',
-                'price': 250,
-                'name': 'Estruturas de Dados em C++',
-                'summary': 'Et architecto provident deleniti facere repellat nobis iste. Id facere quia quae dolores dolorem tempore.',
-                'trainer_id': 4,
-                'trainer_name': 'Bernardo',
-            },
-            {
-                'id': 3,
-                'category': 'Natação',
-                'price': 250,
-                'name': 'Estilo Borboleta',
-                'summary': 'Et architecto provident deleniti facere repellat nobis iste. Id facere quia quae dolores dolorem tempore.',
-                'trainer_id': 2,
-                'trainer_name': 'Alberta',
-            },
-        ], 
-        'selected_trainers': [
-            {
-                'id': 2,
-                'name': 'Alberta Almeida',
-                'expertise': 'Programação Web',
-                'presentation': 'Magni qui quod omnis unde et eos fuga et exercitationem. Odio veritatis perspiciatis quaerat qui aut aut aut',
-                'twitter': 'https://twitter.com/alberta_almeida',
-                'facebook': 'https://facebook.com/almeidaalberta',
-                'instagram': 'https://instagram.com/albermeida',
-                'linkedin': 'https://linkedin.com/prof_alberta',
-            },
-            {
-                'id': 3,
-                'name': 'Augusto Avillez',
-                'expertise': 'Marketing',
-                'presentation': 'Repellat fugiat adipisci nemo illum nesciunt voluptas repellendus. In architecto rerum rerum temporibus',
-                'twitter': 'https://twitter.com/augusto_avillez',
-                'facebook': 'https://facebook.com/augustoavillez',
-                'instagram': 'https://instagram.com/augillez',
-                'linkedin': 'https://linkedin.com/prof_augusto',
-            },
-            {
-                'id': 1,
-                'name': 'Osmar Tello',
-                'expertise': 'Gestão de Conteúdos',
-                'presentation': 'Voluptas necessitatibus occaecati quia. Earum totam consequuntur qui porro et laborum toro des clara',
-                'twitter': 'https://twitter.com/osmar_tello',
-                'facebook': 'https://facebook.com/osmartello',
-                'instagram': 'https://instagram.com/osmello',
-                'linkedin': 'https://linkedin.com/prof_osmar',
-            },
-        ],
-    }
+        'popular_courses':  course_service.most_popular_courses(POPULAR_COURSES_COUNT),
+        'selected_trainers': trainer_service.selected_trainers(SELECTED_TRAINERS_COUNT)
+    })
 #:
 
 @router.get('/about')                        # type: ignore
 @template()
 async def about(request: Request):
-    return {
-        'num_courses': 199,
-        'num_students': 2315,
-        'num_trainers': 23,
+    return about_viewmodel()
+#:
+    
+def about_viewmodel():
+    return base_viewmodel_with({
+        'num_courses': course_service.course_count(),
+        'num_students': student_service.student_count(),
+        'num_trainers': trainer_service.trainer_count(),
         'num_events': 159,
-        'testimonials': [
-            {
-                'user_id': 239,
-                'user_name': 'Saul Goodman',
-                'user_occupation': 'CEO & Founder',
-                'text': 'Quidem odit voluptate, obcaecati, explicabo nobis corporis perspiciatis nihil doloremque eius omnis officia voluptates, facere saepe quas consequatur aliquam unde. Ab numquam reiciendis sequi.',
-            },
-            {
-                'user_id': 1001,
-                'user_name': 'Sara Wilson',
-                'user_occupation': 'Designer',
-                'text': 'Export tempor illum tamen malis malis eram quae irure esse labore quem cillum quid cillum eram malis quorum velit fore eram velit sunt aliqua noster fugiat irure amet legam anim culpa.',
-            },
-            {
-                'user_id': 704,
-                'user_name': 'Jena Karlis',
-                'user_occupation': 'Store Owner',
-                'text': 'Enim nisi quem export duis labore cillum quae magna enim sint quorum nulla quem veniam duis minim tempor labore quem eram duis noster aute amet eram fore quis sint minim.',
-            },
-            {
-                'user_id': 1002,
-                'user_name': 'Matt Brandon',
-                'user_occupation': 'Freelancer',
-                'text': 'Fugiat enim eram quae cillum dolore dolor amet nulla culpa multos export minim fugiat minim velit minim dolor enim duis veniam ipsum anim magna sunt elit fore quem dolore labore illum veniam.',
-            },
-            {
-                'user_id': 1589,
-                'user_name': 'John Larson',
-                'user_occupation': 'Entrepreneur',
-                'text': 'Quis quorum aliqua sint quem legam fore sunt eram irure aliqua veniam tempor noster veniam enim culpa labore duis sunt culpa nulla illum cillum fugiat legam esse veniam culpa fore nisi cillum quid.',
-            },
-        ]
-    }
+        'testimonials': student_service.get_testimonials(TESTIMONIALS_COUNT)
+    })
 #:
