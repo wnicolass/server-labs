0666bada24bf8d0cddaf6f18b1b438831794fb57
wnicolass <nicolaswgl99@gmail.com>
2023-03-09 18:53:56 +0000

    feat: create splice with named args

--- js/functions.js
+++ js/functions.js
@@ -57,4 +57,24 @@ function concatV(items, { sep = "", end = "" } = {}) {
   return itemsTxt.join(sep);
 }
 
-console.log(concatV([1, 2, "alberto", "antunes", [0, 1]], { end: "theend" }));
+// console.log(concatV([1, 2, "alberto", "antunes", [0, 1]], { end: "theend" }));
+
+// splice
+// array.splice(start[, deleteCount[, item1[, item2[, ...]]]])
+
+function splice(items, spliceOptions) {
+  let { start, deleteCount, newItems } = spliceOptions;
+  let result =
+    deleteCount && newItems
+      ? items.splice(start, deleteCount, ...newItems)
+      : deleteCount
+      ? items.splice(start, deleteCount)
+      : newItems
+      ? items.splice(start, 0, ...newItems)
+      : items.splice(start);
+  return result;
+}
+
+let nums = [14, 7, 7, 0, 1];
+splice(nums, { start: 1, deleteCount: 2, newItems: [8, 8, 8] });
+console.log(nums);
