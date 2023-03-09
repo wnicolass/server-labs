a476178efc261b78a787151983b62d5430a46c14
wnicolass <nicolaswgl99@gmail.com>
2023-03-09 20:02:39 +0000

    feat: end js interlude pt b

--- js/functions.js
+++ js/functions.js
@@ -75,6 +75,122 @@ function splice(items, spliceOptions) {
   return result;
 }
 
-let nums = [14, 7, 7, 0, 1];
-splice(nums, { start: 1, deleteCount: 2, newItems: [8, 8, 8] });
-console.log(nums);
+// let nums = [14, 7, 7, 0, 1];
+// splice(nums, { start: 1, deleteCount: 2, newItems: [8, 8, 8] });
+// console.log(nums);
+
+///////////////////////////////////////////////////////
+//
+//   LAMBDAS, ARROW FUNCTIONS & HIGH-ORDER FUNCTIONS
+//
+//////////////////////////////////////////////////////
+
+const nums = [100, 15, -2, -1, 59, 44, 12, 10, 46, 77, 15, 90, 15];
+const names = ["alberto", "bruno", "armando", "josé", "albertina", "amanda"];
+
+// high-order function
+function filter(items, criteria) {
+  const selected = [];
+  for (let item of items) {
+    if (criteria(item)) {
+      selected.push(item);
+    }
+  }
+  return selected;
+}
+
+function ePar(num) {
+  return num % 2 === 0;
+}
+
+function ePositivo(num) {
+  return num >= 0;
+}
+
+function terminadoEmOh(texto) {
+  return texto[texto.length - 1] === "o";
+}
+
+function temPeloMenosTresAs(texto) {
+  let quantos = 0;
+  for (let ch of [...texto]) {
+    if (ch === "a") {
+      quantos += 1;
+    }
+  }
+  return quantos >= 3;
+}
+
+console.log(filter(nums, ePar));
+console.log(
+  filter(nums, function (num) {
+    return num % 2 === 0;
+  })
+);
+console.log(filter(nums, ePositivo));
+console.log(
+  filter(nums, function (num) {
+    return num >= 0;
+  })
+);
+console.log(filter(names, terminadoEmOh));
+console.log(
+  filter(names, function (name) {
+    return name[name.length - 1] === "o";
+  })
+);
+console.log(filter(names, temPeloMenosTresAs));
+console.log(
+  filter(names, function atLeastThreeA(txt) {
+    let quantos = 0;
+    for (let ch of [...txt]) {
+      if (ch === "a") {
+        quantos += 1;
+      }
+    }
+    return quantos >= 3;
+  })
+);
+
+function between(a, b) {
+  return (num) => num >= a && num <= b;
+}
+
+function notBetween(a, b) {
+  return (num) => num < a || num > b;
+}
+
+function lengthEquals(a) {
+  return (obj) => obj.length === a;
+}
+
+// with arrow functions
+console.log(filter(nums, (num) => num % 2 === 0));
+console.log(filter(nums, (num) => num >= 0));
+console.log(filter(names, (name) => name[name.length - 1] === "o"));
+
+console.log(filter(nums, between(10, 20)));
+console.log(filter(nums, notBetween(10, 20)));
+console.log(filter(nums, (num) => num === 15));
+console.log(filter(names, (name) => name.length === 7));
+console.log(filter(names, lengthEquals(7)));
+
+////////////////////////////////////////////////////////////////////////
+//
+//      FILTER, MAP, FOR-EACH, REDUCE, EVERY, SOME
+//
+////////////////////////////////////////////////////////////////////////
+
+console.log(nums.filter((num) => num >= 15));
+console.log(nums.map((num) => num * 2));
+names.forEach((name) => console.log(name));
+names.forEach(console.log);
+names.forEach((name, i) => console.log(`${i} -> ${name}`));
+
+console.log(nums.reduce((acc, num) => acc + num, 0));
+console.log(names.reduce((acc, name) => acc + name + "/", ""));
+
+let allPositive = true;
+allPositive = nums.every((num) => num >= 0);
+let atLeastOnePositive = nums.some((num) => num >= 0);
+console.log(allPositive, atLeastOnePositive);
