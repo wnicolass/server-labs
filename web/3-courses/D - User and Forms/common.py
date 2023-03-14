f8df100bfc3a00a686460bf618b20e38fa075c63
wnicolass <nicolaswgl99@gmail.com>
2023-03-14 10:27:43 +0000

    feat: validate name field

--- web/3-courses/D - User and Forms/common.py
+++ web/3-courses/D - User and Forms/common.py
@@ -1,3 +1,14 @@
+__all__ = (
+    'base_viewmodel',
+    'base_viewmodel_with',
+    'is_valid_name',
+    'form_field_as_str',
+    'form_field_as_file'
+)
+
+from fastapi import UploadFile
+from fastapi.datastructures import FormData
+
 def base_viewmodel():
     return {
         'error': None,
@@ -12,3 +23,18 @@ def base_viewmodel_with(update_data: dict) -> dict:
     vm.update(update_data)
     return vm
 #:
+
+def form_field_as_str(form_data: FormData, field_name: str) -> str:
+    field_value = form_data[field_name]
+    if isinstance(field_value, str):
+        return field_value
+    raise TypeError(f'Form field {field_name} type is not str.')
+
+def form_field_as_file(form_data: FormData, field_name: str) -> UploadFile:
+    field_value = form_data[field_name]
+    if isinstance(field_value, UploadFile):
+        return field_value
+    raise TypeError(f'Form field {field_name} type is UploadFile.')
+
+def is_valid_name(name: str) -> bool:
+    return all(part.isalphe() and len(part) > 2 for part in name.split())
--- web/3-courses/D - User and Forms/views/account.py
+++ web/3-courses/D - User and Forms/views/account.py
@@ -1,22 +1,58 @@
-from fastapi import APIRouter
+from datetime import date
+from fastapi import APIRouter, Request
 from fastapi_chameleon import template
-from common import base_viewmodel_with
+from common import base_viewmodel_with, is_valid_name, form_field_as_file, form_field_as_str
 
 router = APIRouter()
 
-@router.get('/account')                            # type: ignore
+MIN_DATE = date.fromisoformat('1920-01-01')
+
+@router.get('/account')
 async def index():
     return {
 
     }
 #:
 
-@router.get('/account/register')                            # type: ignore
+@router.get('/account/register')
 @template()
 async def register():
+    return register_viewmodel()
+#:
+
+def register_viewmodel():
     return base_viewmodel_with({
-        'error': False,
-        'error_msg': 'There was an error with your data. Please try again.'
+        'name': '',
+        'email': '',
+        'password': '',
+        'birth_date': '',
+        'checked': False,
+        'min_date': MIN_DATE,
+        'max_date': date.today(),
+    })
+#:
+
+@router.post('/account/register')
+@template(template_file = 'account/register.pt')
+async def register(request: Request):
+    return post_register_viewmodel(request)
+#:
+
+async def post_register_viewmodel(request: Request):
+    form_data = await request.form()
+    name = form_field_as_str(form_data, 'name')
+    email = form_field_as_str(form_data, 'name')
+
+    if not is_valid_name(name):
+        error, error_msg = True, 'Nome inválido!'
+    else:
+        error, error_msg = False, ''
+
+    return base_viewmodel_with({
+        'name': '',
+        'email': '',
+        'password': '',
+        'checked': ''
     })
 #:
 
