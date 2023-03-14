d36da6ef8299db5705c5287eeb5a0eea8c2fd199
wnicolass <nicolaswgl99@gmail.com>
2023-03-14 09:46:20 +0000

    fix: missing data

--- web/3-courses/C - Views and Services - 2/views/account.py
+++ web/3-courses/C - Views and Services - 2/views/account.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter
 from fastapi_chameleon import template
+from common import base_viewmodel_with
 
 router = APIRouter()
 
@@ -13,18 +14,18 @@ async def index():
 @router.get('/account/register')                            # type: ignore
 @template()
 async def register():
-    return {
+    return base_viewmodel_with({
         'error': False,
         'error_msg': 'There was an error with your data. Please try again.'
-    }
+    })
 #:
 
 @router.get('/account/login')                            # type: ignore
 @template()
 async def login():
-    return {
+    return base_viewmodel_with({
         'error': False,
         'error_msg': 'There was an error with your data. Please try again.'
-    }
+    })
 #:
 
